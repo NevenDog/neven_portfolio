af8f564b0a78c0d2fbbd934d2e55404fd6fdd98b
NevenDog <mattiacimo777@gmail.com>
2024-11-17 11:09:07 +0100

    changed Models to models in assets folder, modified glb directories

--- vite.config.js
+++ vite.config.js
@@ -4,8 +4,10 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   base: "/",
+  //base: "neven_portfolio",
   plugins: [react()],
   build: {
+    outDir: "dist",
     assetsDir: 'assets',
   }
 })
